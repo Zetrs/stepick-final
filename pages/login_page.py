cf57cb7f6d3408b2f1d5040b23b7ab2ff2065a28
Zetrs <zetrs@yandex.ru>
2021-06-28 14:20:30 +0300

    login page init

---
+++ pages/login_page.py
@@ -0,0 +1,16 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+class LoginPage(BasePage):
+  def should_be_login_page(self):
+    self.should_be_login_url()
+    self.should_be_login_form()
+    self.should_be_register_form()
+  def should_be_login_url(self):
+    index = browser.current_url.find('login')
+    assert index != -1, "Not login URL"
+  def should_be_login_form(self):
+    assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+  def should_be_register_form(self):
+    assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+    
