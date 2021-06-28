d32fd8dd3ba68e843bd7c2604b74d43ebc754e5c
Zetrs <zetrs@yandex.ru>
2021-06-28 17:39:11 +0300

    4_3_8

--- pages/locators.py
+++ pages/locators.py
@@ -1,5 +1,8 @@
 from selenium.webdriver.common.by import By
 
+class BasePageLocators():
+  LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+  LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 class MainPageLocators():
   LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 class LoginPageLocators ():
@@ -11,3 +14,4 @@ class ProductPageLocators ():
   PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
   BASKET_SUM = (By.CSS_SELECTOR, ".alert.alert-info .alertinner p strong")
   ADDED_PRODUCT_NAME = (By.CSS_SELECTOR, ".alertinner strong")
+  SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alertinner")
--- pages/product_page.py
+++ pages/product_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import BasePageLocators
 from .locators import ProductPageLocators
 import time
 
@@ -6,6 +7,9 @@ class ProductPage(BasePage):
   def should_add2basket(self):
     add2basket_link=self.browser.find_element(*ProductPageLocators.ADD_2_CARD_BTN)
     add2basket_link.click()
+
+  def should_add2basket_chk(self):
+    self.should_add2basket()
     self.solve_quiz_and_get_code()
     name=self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
     price=self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
@@ -13,4 +17,17 @@ class ProductPage(BasePage):
     added_product_name=self.browser.find_element(*ProductPageLocators.ADDED_PRODUCT_NAME).text
     assert price == basket_sum, "Basket sum not eaqual to Product price"
     assert name == added_product_name, "Wrong product added to cart"
+  
+  def should_not_be_success_message_after_adding_product_to_basket(self):
+    self.should_add2basket()
+    self.solve_quiz_and_get_code()
+    assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented after added to cart, but should not be"
+  def should_not_be_success_message(self):
+    assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
+  def success_message_disappeared_after_adding_product_to_basket(self):
+    self.should_add2basket()
+    self.solve_quiz_and_get_code()
+    assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
+
+  
     
