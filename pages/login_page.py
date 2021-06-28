7faf41508e93431121d049153e49ed3ba5be9455
Zetrs <zetrs@yandex.ru>
2021-06-28 14:43:38 +0300

    login page init2

--- pages/login_page.py
+++ pages/login_page.py
@@ -7,7 +7,7 @@ class LoginPage(BasePage):
     self.should_be_login_form()
     self.should_be_register_form()
   def should_be_login_url(self):
-    index = browser.current_url.find('login')
+    index = self.browser.current_url.find('login')
     assert index != -1, "Not login URL"
   def should_be_login_form(self):
     assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -2,7 +2,7 @@ from pages.main_page import MainPage
 from pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
@@ -10,7 +10,7 @@ def test_guest_can_go_to_login_page(browser):
     login_page.should_be_login_page()
   
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
